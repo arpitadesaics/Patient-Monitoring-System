c8f4232c8fb4b92634e686c2dab31d086f4b9689
Arpita Sangameswarsarj Desai <39876133+arpitadesaics@users.noreply.github.com>
2024-07-27 17:54:18 -0400

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// vite.config.js
 export default {
-  build: {
-    outDir: 'dist'
-  }
+  base: './', // Adjust this if needed
+  // Other Vite configurations
+}
 }
