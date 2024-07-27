9554e05f268a8cbe8f697756713117690a502094
ArpiDesai <desai.ar@northeastern.edu>
2024-07-27 17:31:13 -0400

    Frontend updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-
-  server: {
-    port: 3000
+export default {
+  build: {
+    outDir: 'dist'
   }
-})
+}
